6176a8e5a359736a386a976e8655430b8eeeb04b
xDark <19853368+xxDark@users.noreply.github.com>
2020-07-29 19:48:37 +0300

    Avoid copying until we are sure that the packet is valid

--- protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java
@@ -32,59 +32,48 @@ public class MinecraftDecoder extends MessageToMessageDecoder<ByteBuf>
 
         int originalReaderIndex = in.readerIndex();
         int originalReadableBytes = in.readableBytes();
-        ByteBuf slice = null; // Can't slice this one due to EntityMap :(
 
-        try
+        int packetId = DefinedPacket.readVarInt( in );
+
+        if ( server && !forge && ( packetId < 0 || packetId > Protocol.MAX_PACKET_ID ) )
         {
-            int packetId = DefinedPacket.readVarInt( in );
-
-            if ( server && !forge && ( packetId < 0 || packetId > Protocol.MAX_PACKET_ID ) )
-            {
-                ChannelUtil.shutdownChannel( ctx.channel(), null );
-                return;
-            }
-            slice = in.copy( originalReaderIndex, originalReadableBytes );
-
-            DefinedPacket packet = prot.createPacket( packetId, protocolVersion );
-            if ( packet != null )
-            {
-                try
-                {
-                    packet.read( in, prot.getDirection(), protocolVersion );
-                } catch ( Throwable t )
-                {
-                    if ( server )
-                    {
-                        ChannelUtil.shutdownChannel( ctx.channel(), t );
-                        return;
-                    }
-                    throw t;
-                }
-
-                if ( in.isReadable() )
-                {
-                    if ( server )
-                    {
-                        ChannelUtil.shutdownChannel( ctx.channel(), new BadPacketException( "Did not read all bytes from packet " + packet.getClass() + " " + packetId + " Protocol " + protocol + " Direction " + prot.getDirection() ) );
-                        return;
-                    } else
-                    {
-                        throw new BadPacketException( "Did not read all bytes from packet " + packet.getClass() + " " + packetId + " Protocol " + protocol + " Direction " + prot.getDirection() );
-                    }
-                }
-            } else
-            {
-                in.skipBytes( in.readableBytes() );
-            }
-
-            out.add( new PacketWrapper( packet, slice ) );
-            slice = null;
-        } finally
-        {
-            if ( slice != null )
-            {
-                slice.release();
-            }
+            ChannelUtil.shutdownChannel( ctx.channel(), null );
+            return;
         }
+
+        DefinedPacket packet = prot.createPacket( packetId, protocolVersion );
+        if ( packet != null )
+        {
+            try
+            {
+                packet.read( in, prot.getDirection(), protocolVersion );
+            } catch ( Throwable t )
+            {
+                if ( server )
+                {
+                    ChannelUtil.shutdownChannel( ctx.channel(), t );
+                    return;
+                }
+                throw t;
+            }
+
+            if ( in.isReadable() )
+            {
+                if ( server )
+                {
+                    ChannelUtil.shutdownChannel( ctx.channel(), new BadPacketException( "Did not read all bytes from packet " + packet.getClass() + " " + packetId + " Protocol " + protocol + " Direction " + prot.getDirection() ) );
+                    return;
+                } else
+                {
+                    throw new BadPacketException( "Did not read all bytes from packet " + packet.getClass() + " " + packetId + " Protocol " + protocol + " Direction " + prot.getDirection() );
+                }
+            }
+        } else
+        {
+            in.skipBytes( in.readableBytes() );
+        }
+
+        ByteBuf slice = in.copy( originalReaderIndex, originalReadableBytes );
+        out.add( new PacketWrapper( packet, slice ) );
     }
 }
